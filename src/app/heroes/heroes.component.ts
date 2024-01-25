0e5af8ea263a380f06f3bf90ebb8ee83460d25e8
stephend-io <127882148+stephend-io@users.noreply.github.com>
2024-01-25 13:30:51 +0800

    Lesson 3: Decoupled hero details into its own component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,19 +4,19 @@ import { AppComponent } from '../app.component';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [AppComponent, UpperCasePipe, FormsModule, NgFor, NgIf],
+  imports: [AppComponent, UpperCasePipe, FormsModule, NgFor, NgIf, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 
 export class HeroesComponent {
-
   heroes = HEROES;
-  selectedHero?: Hero;
+  selectedHero!: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
