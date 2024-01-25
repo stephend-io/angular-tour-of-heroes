75199098dfa273e7c14650299aa03465f862bac8
stephend-io <127882148+stephend-io@users.noreply.github.com>
2024-01-25 13:19:14 +0800

    Lesson 2: Added mock data, and detailed list of heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
 
+
 @Component({
     selector: 'app-root',
     standalone: true,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { AppComponent } from '../app.component';
-import { UpperCasePipe } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [AppComponent, UpperCasePipe, FormsModule],
+  imports: [AppComponent, UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
